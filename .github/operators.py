8a289cbe647e4da4097304bdd60d39da4160563d
mateen39 <157426991+mateen39@users.noreply.github.com>
2024-01-26 14:48:08 -0800

    Create operators.py

---
+++ .github/operators.py
@@ -0,0 +1,5 @@
+song= 2 + 1
+song= 3.0 
+song= "geese" 
+print(song) 
+# This code is assignning different values to the same variable. The last assignment using the '=' operator is the "geese" string that is the last in memory allocation by the program.
